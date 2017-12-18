532b5ea7d4b8d8170b5753e85babe500bc5b893c
Yerson Andres Escobar Maturana <yescobar@alumnos.inf.utfsm.cl>
2017-12-18 05:12:20 -0300

    Prueba del yml

--- inf323-utfsm-grupo02/settings/production.py
+++ inf323-utfsm-grupo02/settings/production.py
@@ -73,12 +73,25 @@ WSGI_APPLICATION = 'inf323-utfsm-grupo02.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DATABASE_NAME', ''),
+        'USER': os.environ.get('DATABASE_USER', ''),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', ''),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
